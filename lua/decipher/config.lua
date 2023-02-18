700661472a6440581e304cd33aeacd17631a51b6
MisanthropicBit <albo.developer@gmail.com>
2023-02-18 23:11:42 +0100

    Validate configs

--- lua/decipher/config.lua
+++ lua/decipher/config.lua
@@ -48,6 +48,61 @@ local default_config = {
     },
 }
 
+--- Validate a dimension
+local function validate_dimension(arg)
+    return arg == "auto" or type(arg) == "number"
+end
+
+--- Validate a codec option
+local function validate_codecs(arg)
+    if arg == "all" then
+        return true
+    elseif type(arg) == "table" then
+        for idx, value in ipairs(arg) do
+            if codecs.get(value) == nil then
+                return false, string.format("Invalid codec '%s' at index %d in config.active_codecs", value, idx)
+            end
+        end
+
+        return true
+    end
+
+    return false, 'config.active_codecs should be "all" or a list of codecs'
+end
+
+--- Validate a floating window border
+local function validate_border(arg)
+    local presets = {
+        "none",
+        "single",
+        "double",
+        "rounded",
+        "solid",
+        "shadow",
+    }
+
+    return presets[arg] ~= nil or type(arg) == "table"
+end
+
+--- Validate a config
+---@param _config decipher.Config
+local function validate_config(_config)
+    vim.validate({
+        active_codecs = { _config.active_codecs, validate_codecs, "valid codecs" },
+        ["float.max_width"] = { _config.float.max_width, validate_dimension, "valid dimension" },
+        ["float.max_height"] = { _config.float.max_height, validate_dimension, "valid dimension" },
+        ["float.padding"] = { _config.float.padding, "number" },
+        ["float.border"] = { _config.float.border, validate_border, "valid border" },
+        ["float.dismiss"] = { _config.float.dismiss, "string" },
+        ["float.apply"] = { _config.float.apply, "string" },
+        ["float.title"] = { _config.float.title, "boolean" },
+        ["float.title_separator"] = { _config.float.title_separator, "string" },
+        ["float.autoclose"] = { _config.float.autoclose, "boolean" },
+        ["float.enter"] = { _config.float.enter, "boolean" },
+        ["float.options"] = { _config.float.options, "table" },
+    })
+end
+
 ---@type decipher.Config
 local _user_config = default_config
 
@@ -58,6 +113,8 @@ function config.setup(user_config)
     if _user_config.active_codecs == "all" then
         _user_config.active_codecs = codecs.supported()
     end
+
+    validate_config(_user_config)
 end
 
 setmetatable(config, {
---
+++ tests/config_spec.lua
@@ -0,0 +1,73 @@
+local config = require("decipher.config")
+
+describe("decipher.config", function()
+    it("throws an error on invalid configs", function()
+        local wrong_configs = {
+            { float = { padding = "nope" } },
+            { float = { max_height = "nah" } },
+            { float = { max_width = true } },
+            { float = { max_width = true } },
+            { float = { border = true } },
+            { float = { border = "triple" } },
+            { float = { dismiss = 1 } },
+            { float = { apply = {} } },
+            { float = { title = {} } },
+            { float = { title_separator = 1 } },
+            { float = { autoclose = 1 } },
+            { float = { options = "whoops" } },
+        }
+
+        for _, wrong_config in ipairs(wrong_configs) do
+            assert.has_error(function()
+                ---@diagnostic disable-next-line:assign-type-mismatch
+                config.setup(wrong_config)
+            end)
+        end
+
+        assert.has_error(
+            function()
+                ---@diagnostic disable-next-line:assign-type-mismatch
+                config.setup({ active_codecs = "oops" })
+            end,
+            'active_codecs: expected valid codecs, got oops. Info: config.active_codecs should be "all" or a list of codecs'
+        )
+
+        -- We cannot use an expected error string here since the table's memory
+        -- address is printed as part of the string and is different every time
+        assert.has_error(function()
+            ---@diagnostic disable-next-line:assign-type-mismatch
+            config.setup({ active_codecs = { "base64", "base33" } })
+        end)
+    end)
+
+    it("throws no errors for a valid config", function()
+        config.setup({
+            active_codecs = { "base64", "base32" },
+            float = {
+                max_height = 100,
+                max_width = "auto",
+                padding = 1,
+                border = {
+                    { "╭", "Keyword" },
+                    { "─", "Keyword" },
+                    { "╮", "Keyword" },
+                    { "│", "Keyword" },
+                    { "╯", "Keyword" },
+                    { "─", "Keyword" },
+                    { "╰", "Keyword" },
+                    { "│", "Keyword" },
+                },
+                dismiss = "e",
+                apply = "p",
+                title = false,
+                title_separator = "",
+                autoclose = false,
+                enter = true,
+                options = {
+                    wrap = false,
+                    number = true,
+                },
+            },
+        })
+    end)
+end)

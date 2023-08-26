94289d26095e155915bef943188c522620c59531
MisanthropicBit <albo.developer@gmail.com>
2023-08-26 10:37:15 +0200

    Use has_bit_library function in health check

--- lua/decipher/health.lua
+++ lua/decipher/health.lua
@@ -1,11 +1,10 @@
 local health = {}
+local decipher = require("decipher")
 
 function health.check()
     vim.health.report_start("decipher.nvim")
 
-    local has_bit, _ = pcall(require, "bit")
-
-    if not has_bit then
+    if not decipher.has_bit_library() then
         vim.health.report_error("A bit library is required", {
             "Build neovim with luajit",
             "Use neovim v0.9.0+ which includes a bit library",

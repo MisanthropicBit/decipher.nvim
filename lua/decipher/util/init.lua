67643d6ac63702c9e83ec0a9da14050d4d0d2eb1
MisanthropicBit <proximity.dev@proton.me>
2025-02-10 19:55:22 +0100

    Remove require path to vader and luassert (#24)

--- lua/decipher/util/init.lua
+++ lua/decipher/util/init.lua
@@ -2,5 +2,4 @@ return {
     json = require("decipher.util.json"),
     str = require("decipher.util.string"),
     test = require("decipher.util.test"),
-    vader = require("decipher.util.vader"),
 }

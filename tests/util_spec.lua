a779fd61aa74fc31e537c7d38cfc6fc626b67694
MisanthropicBit <proximity.dev@proton.me>
2026-01-22 12:18:16 +0100

    Test nested json objects

--- tests/util_spec.lua
+++ tests/util_spec.lua
@@ -84,7 +84,9 @@ describe("util", function()
             local test = {
                 msg = "hello",
                 levels = {
-                    b = 2,
+                    b = {
+                        d = 4,
+                    },
                     c = 3,
                     a = 1,
                 },
@@ -100,7 +102,9 @@ describe("util", function()
   "hide": true,
   "levels": {
     "a": 1,
-    "b": 2,
+    "b": {
+      "d": 4
+    },
     "c": 3
   },
   "msg": "hello",
